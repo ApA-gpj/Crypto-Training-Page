be3a4cd29a379e1636a091c60054e3008229f5f9
Peyman Jahangiri <86545970+ApA-gpj@users.noreply.github.com>
2023-01-09 12:02:32 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../src/Components/UI/Container/Container";
+import Header from "./Components/UI/Container/Header/Header";
+import Main from "./Components/UI/Container/Body/Main";
+
+const App = () => {
+  return (
+      <Container>
+        <Header />
+        <Main />
+      </Container>
+  );
+};
+
+export default App;
